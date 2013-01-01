340274625bc37e1325ff6a609ccca7a9f75cfc95
Infocatcher <infocatcher.dev.null@gmail.com>
2013-01-01 21:45:39 +0400

    Bump version to 0.1.0 + some cleanups

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -2,4 +2,4 @@ pref("extensions.addonsQuickSearch.scrollPosition", 50); // Persent: 0..100
 pref("extensions.addonsQuickSearch.saveSelected", 2);
 // 0 - don't save
 // 1 - restore only on about:addons startup
-// 2 - save selected item in each list
+// 2 - save selected item for each list
